f68996040287b1f3df9ae48a11f792f4b9288355
Pratik Dahal <pratikdahal105@gmail.com>
2024-01-14 22:52:13 -0500

    static files collection

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -144,6 +144,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = "media/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
 else:
     urlpatterns += staticfiles_urlpatterns()
