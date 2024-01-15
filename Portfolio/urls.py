653b88d2d7e830ab86e35d4e471d00bf7c277916
Pratik Dahal <pratikdahal105@gmail.com>
2024-01-14 22:43:08 -0500

    static url route

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -18,7 +18,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
-# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('api.profile.urls')),
@@ -33,7 +33,8 @@ urlpatterns = [
 ]
 
 
-# if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# else:
-#     urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += staticfiles_urlpatterns()
