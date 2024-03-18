ee85e7b84d308cf7eb07e1fc11ef00721c56430f
Pratik Dahal <pratikdahal105@gmail.com>
2024-03-18 12:18:56 -0400

    message fetch fix updated serializer

--- api/contact/serializers.py
+++ api/contact/serializers.py
@@ -3,13 +3,18 @@ from contact.models import Contact
 from django.contrib.auth.models import User
 
 class ContactSerializer(serializers.ModelSerializer):
+    subject = serializers.CharField(max_length=255, write_only=True)  # Add subject field as write-only
+
     class Meta:
         model = Contact
-        fields = ['id', 'mail_from', 'content', 'full_name']
+        fields = ['id', 'mail_from', 'content', 'subject']  # Replace 'full_name' with 'subject'
 
     def create(self, validated_data):
         # Retrieve the user from the context
         user = self.context.get('user')
+
+        # Manipulate the validated data to store subject in place of full_name
+        validated_data['full_name'] = validated_data.pop('subject')
 
         # Set the user field of the contact instance
         validated_data['user'] = user
@@ -20,6 +25,6 @@ class ContactSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         instance.mail_from = validated_data.get('mail_from', instance.mail_from)
         instance.content = validated_data.get('content', instance.content)
-        instance.full_name = validated_data.get('full_name', instance.full_name)
+        instance.full_name = validated_data.get('subject', instance.full_name)  # Update subject field
         instance.save()
         return instance
